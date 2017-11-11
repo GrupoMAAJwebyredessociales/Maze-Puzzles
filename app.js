b4f6ada7fa128fae06c9fe36e38925cae4934b57
AngeelBG <32101206+AngeelBG@users.noreply.github.com>
2017-11-11 16:54:24 +0100

    Menus
    Distintos menus y navegacion entre ellos

---
+++ app.js
@@ -0,0 +1,58 @@
+// función que permite poner visible o no una sección div, asi conseguimos que
+// la tabla se oculte cuando inciamos partida
+function ponerVisible(div, visible) {
+	let
+	estado = visible ? "block" : "none"; // block para que se vea, o none
+											// para que no se vea
+	div.css({
+		"display" : estado
+	});
+}
+
+// función que cambia de estado a bloqueado y viceversa (uso de botones)
+function cambioEstadoBloqueado(boton, nuevoEstado){
+	if(nuevoEstado){// true para activarlo
+		boton.css({"opacity":1}); // opacity 1 visible
+		boton.prop("disabled", false);
+	}else{
+		boton.css({"opacity":0.5}); // opacity 0,5 no puede usarse
+		boton.prop("disabled", true);
+		}
+}
+
+
+// funcion que se invoca al cargar la pagina
+$(function() {
+    
+    // BOTONES MENUS DEL JUEGO 
+    $("#Bjugar").click(
+			function() {
+                    ponerVisible($("#menu"), false);
+                    ponerVisible($("#menuJugar"), true);
+                
+				})
+    $("#Binstrucciones").click(
+			function() {
+                    ponerVisible($("#menu"), false);
+                    ponerVisible($("#menuInstrucciones"), true);
+				})
+    
+    $("#Bcontactar").click(
+			function() {
+                    ponerVisible($("#menu"), false);
+                    ponerVisible($("#menuContactar"), true);
+				})
+  
+    $("#BatrasIns").click(
+			function() {
+                    ponerVisible($("#menu"), true);
+                    ponerVisible($("#menuInstrucciones"), false);
+				})
+    $("#BatrasCon").click(
+			function() {
+                    ponerVisible($("#menu"), true);
+                    ponerVisible($("#menuContactar"), false);
+				})
+
+    
+})
